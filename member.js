749f2bb0252397ba6d1fc99bbb84b6ffe11a1cff
anazite <108135661+anazite@users.noreply.github.com>
2023-12-26 06:54:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2) {
+    console.log(var1 + var2);
+}
+
+function skillsMember(name, age, ...skills) {
+    console.log(`Name: ${name}, Age: ${age}, Skills: ${skills}`);
+}
